a29ae276c50307abbcb69a0eedbc58c16fca7bf1
victoria <victoriayvu@gmail.com>
2024-04-10 15:05:58 -0400

    badoosh

---
+++ main.py
@@ -0,0 +1,26 @@
+class lab_9:
+    def start(self):
+
+        while True:
+            self.menu()
+            option = int(input("Please enter an option: "))
+            if option == "1":
+                encode = int(input("Please enter your password to encode: ")
+
+
+
+    def menu(self):
+        print("Menu")
+        print("-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+        return
+
+
+if __name__ == "__main__":
+    while True:
+        menu()
+    pass
+
+
