b612c4911a902fe5d6ec3abaaae33c83eb267c1c
victoria <victoriayvu@gmail.com>
2024-04-10 15:16:27 -0400

    badoosh

--- main.py
+++ main.py
@@ -1,26 +1,23 @@
-class lab_9:
-    def start(self):
-
-        while True:
-            self.menu()
-            option = int(input("Please enter an option: "))
-            if option == "1":
-                encode = int(input("Please enter your password to encode: ")
-
-
-
-    def menu(self):
-        print("Menu")
-        print("-------------")
-        print("1. Encode")
-        print("2. Decode")
-        print("3. Quit")
-        return
+def start():
+    while True:
+        menu()
+        option = int(input("\nPlease enter an option: "))
+        if option == "1":
+            num = int(input("Please enter your password to encode: "))
+            encode()
+        elif option == "2":
+            decode()
+            print(f"The encoded password is {num}, and the original password is {num}.")
+        elif option == "3":
+            break
+def menu():
+    print("\nMenu")
+    print("-------------")
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Quit")
+    return
 
 
 if __name__ == "__main__":
-    while True:
-        menu()
-    pass
-
-
+    start()
