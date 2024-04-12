c1d27342bfe4411f7386f455494e816b3fcedf57
victoriayvu <166648948+victoriayvu@users.noreply.github.com>
2024-04-12 14:12:29 -0400

    edited encoder and start

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
+
+def encode(num):
+    new_number = ""
+    for digit in str(num):
+        add_three = str(int(digit) + 3)
+        new_number += add_three
+    return new_number
+
+def decoder(new_number):
+    result = ''
+    for digit in str(new_number):
+        new_digit = str((int(digit) - 3) % 10)
+        result += new_digit
+    return result
+
 def start():
     while True:
         menu()
         option = int(input("\nPlease enter an option: "))
-        if option == "1":
+        if option == 1:
             num = int(input("Please enter your password to encode: "))
-            encode()
+            encoded = encode(num)
             print("Your password has been encoded and stored!")
-        elif option == "2":
-            decoder()
-            print(f"The encoded password is {xxx}, and the original password is {num}.")
-        elif option == "3":
+        elif option == 2:
+            decoder(encoded)
+            print(f"The encoded password is {encoded}, and the original password is {num}.")
+        elif option == 3:
             break
 def menu():
     print("\nMenu")
@@ -18,7 +33,6 @@ def menu():
     print("2. Decode")
     print("3. Quit")
     return
-
 
 
 if __name__ == "__main__":
