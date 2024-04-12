0f300dc18859e5fba20965dbf21be2b2dc320642
lilianguolil <166648977+lilianguolil@users.noreply.github.com>
2024-04-12 01:01:32 -0400

    added decoder function and fixed start function and menu selection

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
+def decoder(new_number):
+    result = ''
+    for digit in new_number:
+        new_digit = str((int(digit) - 3) % 10)
+        result += new_digit
+    return result
+
 def start():
     while True:
         menu()
         option = int(input("\nPlease enter an option: "))
         if option == "1":
             num = int(input("Please enter your password to encode: "))
-            encode()
+            encoded = encode(num)
             print("Your password has been encoded and stored!")
         elif option == "2":
-            decoder()
-            print(f"The encoded password is {xxx}, and the original password is {num}.")
+            original = decoder(encoded)
+            print(f"The encoded password is {encoded}, and the original password is {original}.")
         elif option == "3":
             break
 def menu():
@@ -18,6 +25,7 @@ def menu():
     print("2. Decode")
     print("3. Quit")
     return
+
 
 
 if __name__ == "__main__":
